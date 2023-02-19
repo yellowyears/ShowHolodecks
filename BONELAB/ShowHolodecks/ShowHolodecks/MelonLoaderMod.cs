30eccc973a31340c3cf049cf7f4aa87aec4eecfc
yellowyears <yellowyears200@gmail.com>
2023-02-19 23:56:39 +0000

    Add ShowHolodecks mod

---
+++ BONELAB/ShowHolodecks/ShowHolodecks/MelonLoaderMod.cs
@@ -0,0 +1,84 @@
+﻿using Il2CppSystem.Collections.Generic;
+using MelonLoader;
+using UnhollowerBaseLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ShowHolodecks
+{
+    public static class BuildInfo
+    {
+        public const string Name = "ShowHolodecks"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "yellowyears"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class ShowHolodecks : MelonMod
+    {
+
+        private string targetSceneName = "9845994255863274994b7d033e3bdc76";
+        private string targetRootName = "//-----ENVIRONMENT";
+        private string targetObjectName = "HOLODECKS";
+
+        private GameObject holodeckParent = null;
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == targetSceneName)
+            {
+                MelonLogger.Msg("BONELAB Hub loaded: Activating HoloDecks");
+
+                ActivateHolodeckParent();
+                DisableSkinnedMeshRenderers(holodeckParent);
+            }
+        }
+
+        private void ActivateHolodeckParent()
+        {
+            var rootObjects = GetSceneRootObjects(targetSceneName);
+
+            foreach (var rootObject in rootObjects)
+            {
+                rootObject.Cast<GameObject>();
+                if (rootObject.name == targetRootName)
+                {
+                    holodeckParent = GetObjectInChildren(targetObjectName, rootObject);
+                    break;
+                }
+            }
+
+            holodeckParent.SetActive(true);
+        }
+
+        private void DisableSkinnedMeshRenderers(GameObject targetObject)
+        {
+            var renderers = targetObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+            foreach (var renderer in renderers)
+            {
+                renderer.enabled = false;
+            }
+        }
+
+        private Il2CppReferenceArray<GameObject> GetSceneRootObjects(string sceneName)
+        {
+            var scene = SceneManager.GetSceneByName(sceneName);
+            return scene.GetRootGameObjects();
+        }
+
+        private GameObject GetObjectInChildren(string targetObjectName, GameObject parentGameObject)
+        {
+            for (var i = 0; i < parentGameObject.transform.GetChildCount(); i++)
+            {
+                var childGameObject = parentGameObject.transform.GetChild(i);
+                if (childGameObject.name == targetObjectName)
+                {
+                    return childGameObject.gameObject;
+                }
+            }
+            return null;
+        }
+
+    }
+}
---
+++ BONELAB/ShowHolodecks/ShowHolodecks/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(ShowHolodecks.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(ShowHolodecks.BuildInfo.Company)]
+[assembly: AssemblyProduct(ShowHolodecks.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + ShowHolodecks.BuildInfo.Author)]
+[assembly: AssemblyTrademark(ShowHolodecks.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(ShowHolodecks.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(ShowHolodecks.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(ShowHolodecks.ShowHolodecks), ShowHolodecks.BuildInfo.Name, ShowHolodecks.BuildInfo.Version, ShowHolodecks.BuildInfo.Author, ShowHolodecks.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
