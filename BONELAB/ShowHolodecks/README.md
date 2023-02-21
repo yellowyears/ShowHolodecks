91aef4acd9b65e043a127b1e992e49d7717ea4ca
yellowyears <yellowyears200@gmail.com>
2023-02-21 22:04:14 +0000

    Refactor ShowHolodecks.cs and add README

---
+++ BONELAB/ShowHolodecks/README.md
@@ -0,0 +1,7 @@
+# Show Holodecks
+
+Enables the Holodecks found in the BONELAB Hub and fixes some positioning issues
+
+![Example](example.jpg)
+
+[Mod Link](https://bonelab.thunderstore.io/package/yellowyears/ShowHolodecks/)
--- BONELAB/ShowHolodecks/ShowHolodecks/ShowHolodecks.cs
+++ BONELAB/ShowHolodecks/ShowHolodecks/ShowHolodecks.cs
@@ -10,73 +10,80 @@ namespace ShowHolodecks
         public const string Name = "ShowHolodecks"; // Name of the Mod.  (MUST BE SET)
         public const string Author = "yellowyears"; // Author of the Mod.  (Set as null if none)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
-        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+        public const string Version = "1.0.1"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = "https://bonelab.thunderstore.io/package/yellowyears/ShowHolodecks/"; // Download Link for the Mod.  (Set as null if none)
     }
 
     public class ShowHolodecks : MelonMod
     {
-
-        private readonly string _targetSceneName = "9845994255863274994b7d033e3bdc76";
-        private readonly string _targetRootName = "//-----ENVIRONMENT";
-        private readonly string _targetObjectName = "HOLODECKS";
-
-        private GameObject _holodeckParent = null;
-
+        // Strings used to identify GameObjects and Scenes
+        private const string TargetSceneName = "9845994255863274994b7d033e3bdc76";
+        private const string TargetRootName = "//-----ENVIRONMENT";
+        private const string TargetObjectName = "HOLODECKS";
+        
+        
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            if (sceneName == _targetSceneName)
-            {
-                MelonLogger.Msg("BONELAB Hub loaded: Activating HoloDecks");
+            if (sceneName != TargetSceneName) return;
+            
+            MelonLogger.Msg("BONELAB Hub loaded: Activating HoloDecks");
 
-                ActivateHolodeckParent();
-                ModifyChildGameObjects(_holodeckParent);
-            }
+            ActivateHolodecks();
         }
-
-        private void ActivateHolodeckParent()
+        
+       
+        private void ActivateHolodecks()
         {
-            var rootObjects = GetSceneRootObjects(_targetSceneName);
+            // Since GameObject.Find() doesn't find inactive objects call GetSceneRootObjects()
+            var rootObjects = GetSceneRootGameObjects(TargetSceneName);
+            var holodeckParent = new GameObject();
 
+            // Loop through all scene root objects until HOLODECK's parent object (//-----ENVIRONMENT) is found
             foreach (var rootObject in rootObjects)
             {
-                rootObject.Cast<GameObject>();
-                if (rootObject.name == _targetRootName)
-                {
-                    _holodeckParent = GetObjectInChildren(_targetObjectName, rootObject);
-                    break;
-                }
+                rootObject.Cast<GameObject>(); // Cast Il2CppReferenceArray<GameObject> element to GameObject
+                if (rootObject.name != TargetRootName) continue;
+                
+                holodeckParent = GetObjectInChildren(TargetObjectName, rootObject); // Get the HOLODECKS GameObject
+                break;
             }
 
-            _holodeckParent.SetActive(true);
+            // Set the parent object to true and apply changes to renderer objects
+            holodeckParent.SetActive(true);
+            ModifyChildGameObjects(holodeckParent);
         }
+        
 
-        private void ModifyChildGameObjects(GameObject targetObject)
+        // Takes a parent object, finds the SkinnedMeshRenderer components, disables them and adjusts the position of them
+        private static void ModifyChildGameObjects(GameObject targetObject)
         {
             var renderers = targetObject.GetComponentsInChildren<SkinnedMeshRenderer>();
             foreach (var renderer in renderers)
             {
                 renderer.enabled = false;
 
+                // Caching
                 var rendererParentTransform = renderer.transform.parent;
-                if (rendererParentTransform.name == "Holodeck_walls_05")
-                {
-                    rendererParentTransform.localPosition = new Vector3(-25.1f, -3.1f, rendererParentTransform.localPosition.z);
-                }
-                else
-                {
-                    rendererParentTransform.localPosition = new Vector3(rendererParentTransform.localPosition.x, -3.1f, rendererParentTransform.localPosition.z);
-                }
+                var rendererParentTransformLocalPos = rendererParentTransform.localPosition;
+                
+                // Reposition the parent object to prevent the holodeck from floating off the ground
+                // Special case for Holodeck_walls_05 (Arena module) since it needs to be pushed backwards on the x axis
+                rendererParentTransform.localPosition = rendererParentTransform.name == "Holodeck_walls_05"
+                    ? new Vector3(-25.1f, -3.1f, rendererParentTransformLocalPos.z)
+                    : new Vector3(rendererParentTransform.localPosition.x, -3.1f, rendererParentTransformLocalPos.z);
             }
         }
-
-        private Il2CppReferenceArray<GameObject> GetSceneRootObjects(string sceneName)
+        
+        // Gets each root GameObject in a scene and return them to an Il2CppReferenceArray of GameObjects
+        private static Il2CppReferenceArray<GameObject> GetSceneRootGameObjects(string sceneName)
         {
             var scene = SceneManager.GetSceneByName(sceneName);
             return scene.GetRootGameObjects();
         }
 
-        private GameObject GetObjectInChildren(string targetObjectName, GameObject parentGameObject)
+        
+        // Returns a specific GameObject that is the child of a parent GameObject
+        private static GameObject GetObjectInChildren(string targetObjectName, GameObject parentGameObject)
         {
             for (var i = 0; i < parentGameObject.transform.GetChildCount(); i++)
             {
